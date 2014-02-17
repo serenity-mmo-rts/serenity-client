409da54318866294a26d6e1f96632a4b055894e9
Holger Finger <hfinger@users.noreply.github.com>
2014-02-17 17:06:15 +0100

    changed class format of mapData.js

--- src/client/mapData.js
+++ src/client/mapData.js
@@ -5,10 +5,10 @@ var MapData = function(layerid,width,height,mapTypeId) {
     this.mapTypeId = 0;
     this.quadTree = new QuadTree({x:0, y:0, width:width, height:height},false);
     this.objects = [];
+}
 
-    this.collisionDetecttion = function (object) {
-        object.width
-        var items = quad.retrieve({x:11, y:20, height:10, width:20});
-        return items;
-    }
+MapData.prototype.collisionDetection = function (object) {
+
+    var items = quad.retrieve({x:object.x, y:object.y, height:object.height, width:object.width});
+    return items;
 }
