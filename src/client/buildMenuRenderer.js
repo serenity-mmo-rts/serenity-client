d294f023065ea547408367723c25e8cad67d2ed3
Holger Finger <hfinger@users.noreply.github.com>
2014-02-23 14:07:32 +0100

    fixed callback and this. references

--- src/client/buildMenuRenderer.js
+++ src/client/buildMenuRenderer.js
@@ -3,7 +3,14 @@
 
 var BuildMenu = function (eventBuild, eventDelete, eventMove, menu_container) {
 
-    var menu_buttons;
+    this.init(eventBuild, eventDelete, eventMove, menu_container)
+
+
+}
+
+BuildMenu.prototype.init = function (eventBuild, eventDelete, eventMove, menu_container) {
+    var self = this;
+    this.menu_buttons = [];
 
     baseMenu_container = new createjs.Container();
     baseMenu_container.name = "menu";
@@ -98,32 +105,30 @@ var BuildMenu = function (eventBuild, eventDelete, eventMove, menu_container) {
     menu_container.addChild(baseMenu_container);
 
     // event listener for main menu
-    baseMenu_container.addEventListener("click", this.click_main_menu);
+    baseMenu_container.addEventListener("click", function () {self.click_main_menu()});
 
 
-    menu_buttons = [delete_button_img, move_button_img, build_button_container, build_menu_container];
-
-
+    this.menu_buttons = [delete_button_img, move_button_img, build_button_container, build_menu_container];
 }
 
 BuildMenu.prototype.click_main_menu = function () {
     nr_child = menu_container.getNumChildren();
     if (nr_child < 3) {
 
-        menu_buttons[0].x = 5;
-        menu_buttons[0].y = canvas_height - 128 - 84;
+        this.menu_buttons[0].x = 5;
+        this.menu_buttons[0].y = canvas_height - 128 - 84;
 
-        menu_buttons[1].x = 64 + 5;
-        menu_buttons[1].y = canvas_height - 128 - 64;
+        this.menu_buttons[1].x = 64 + 5;
+        this.menu_buttons[1].y = canvas_height - 128 - 64;
 
-        menu_buttons[2].x = 128 + 5;
-        menu_buttons[2].y = canvas_height - 128 - 44;
+        this.menu_buttons[2].x = 128 + 5;
+        this.menu_buttons[2].y = canvas_height - 128 - 44;
 
-        menu_container.addChild(menu_buttons[0], menu_buttons[1], menu_buttons[2]);
+        menu_container.addChild(this.menu_buttons[0], menu_buttons[1], menu_buttons[2]);
 
-        menu_buttons[0].addEventListener("click", eventDelete);
-        menu_buttons[1].addEventListener("click", eventMove);
-        menu_buttons[2].addEventListener("click", click_build_menu);
+        this.menu_buttons[0].addEventListener("click", eventDelete);
+        this.menu_buttons[1].addEventListener("click", eventMove);
+        this.menu_buttons[2].addEventListener("click", click_build_menu);
         stage.update();
     }
     else {
