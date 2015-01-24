12e8e69d0917e2170d51fcb1700425362b711569
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2015-01-24 14:41:50 +0100

    RessourceType.js

--- src/client/RessourceMap.js
+++ src/client/RessourceMap.js
@@ -280,6 +280,10 @@ RessourceMap.prototype.genBitmapFromResData = function (resData) {
     return bmp;
 }
 
+
+
+
+
 RessourceMap.prototype.newFilledArray = function (len, val) {
     var rv = new Array(len);
     while (--len >= 0) {
