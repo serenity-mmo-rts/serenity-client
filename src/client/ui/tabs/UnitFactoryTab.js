86b58689e7a43bc7595ebc23cab1f8412d6a87e7
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2015-06-30 19:26:03 +0200

    build units are visible

--- src/client/ui/tabs/UnitFactoryTab.js
+++ src/client/ui/tabs/UnitFactoryTab.js
@@ -2,19 +2,18 @@
 var UnitFactoryTab = function (mapObj) {
 
     this.mapObj = mapObj;
-    this.content= $('<div id="mainTab"></div>');
-    this.createItems();
+    this.content= $('<div id="mainTab"></div>').css({'display': 'inline-block'});
+    this.createUnits();
+    this.availableUnits();
 
 }
 
 
-
-
-
-UnitFactoryTab.prototype.createItems = function () {
+UnitFactoryTab.prototype.createUnits = function () {
     var self = this;
+    var wrap1 = $('<div></div>').css({'display': 'inline-block','padding-right':'20px'});
     var creationTitle =  $('<div>Recruitment</div>').css({});
-    var creationBox =  $('<div></div>').css({'border':'1px solid blue','width':160, 'height':130, 'position': 'relative'});
+    var creationBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
     var allowedItemIds= game.objectTypes.get(this.mapObj.objTypeId)._initProperties._itemIds;
 
 
@@ -27,14 +26,10 @@ UnitFactoryTab.prototype.createItems = function () {
         var y = spriteFrameIcon[1];
         var breite = spriteFrameIcon[2];
         var hoehe = spriteFrameIcon[3];
-        var scale = (100/breite);
         var img = spritesheet.images[spriteFrameIcon[4]];
         var gap = 8;
 
-       // var createItemButton = $('<input id="itemId" type="button" value="build Item"/>').appendTo(this.content);
-
-        var container= $('<div style="white-space:nowrap"></div>').css({'width':32, 'height':32, 'position': 'relative','display': 'inline-block','padding-right':gap+'px'});
-        //var container= $('<div style="white-space:nowrap"></div>').css({'top':'0','left':i,'width':32, 'height':32,'display': 'inline'});
+        var container= $('<div style="white-space:nowrap"></div>').css({'width':breite, 'height':hoehe, 'position': 'relative','display': 'inline-block','padding-right':gap+'px'});
         var image = $('<div style="white-space:nowrap" ></div>')
         image.css({'background-image': 'url('+img+')' ,'background-position-x':-x, 'background-position-y':-y,'background-repeat':'no-repeat','width':breite,'height':hoehe});
         image.appendTo(container);
@@ -50,10 +45,54 @@ UnitFactoryTab.prototype.createItems = function () {
             uc.addEvent(evt);
         });
     }
-    creationTitle.appendTo(this.content)
-    creationBox.appendTo(this.content);
+    creationTitle.appendTo(wrap1);
+    creationBox.appendTo(wrap1);
+    wrap1.appendTo(this.content);
 
 }
 
+
+UnitFactoryTab.prototype.availableUnits = function () {
+    var self = this;
+    var wrap2 = $('<div></div>').css({'display': 'inline-block','padding-right':'20px','top':'200','position': 'absolute'});
+    var availableTitle =  $('<div>Available Units</div>').css({});
+    var availableBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
+    var allItems= this.mapObj.getItems();
+
+    for (var i = 0; i<allItems.length; i++){
+        var itemId = allItems[i]._itemTypeId;
+        var itemType =  game.itemTypes.get(itemId);
+        var type =  itemType._type;
+        var position = allItems[i].getPosition();
+        if (type == "Unit" && position == "Base") {
+
+            var spritesheet = game.spritesheets.get(itemType._iconSpritesheetId);
+            var spriteFrameIcon = spritesheet.frames[itemType._iconSpriteFrame];
+            var x = spriteFrameIcon[0];
+            var y = spriteFrameIcon[1];
+            var breite = spriteFrameIcon[2];
+            var hoehe = spriteFrameIcon[3];
+            var img = spritesheet.images[spriteFrameIcon[4]];
+            var gap = 8;
+
+            var container= $('<div style="white-space:nowrap"></div>').css({'width':32, 'height':32, 'position': 'relative','display': 'inline-block','padding-right':gap+'px'});
+            var image = $('<div style="white-space:nowrap" ></div>')
+            image.css({'background-image': 'url('+img+')' ,'background-position-x':-x, 'background-position-y':-y,'background-repeat':'no-repeat','width':breite,'height':hoehe});
+            image.appendTo(container);
+            container.appendTo(availableBox);
+            var self = this;
+            container.click(function (e) {
+                //e.stopImmediatePropagation();
+                // e.preventDefault();
+            });
+        }
+
+        }
+
+    availableTitle.appendTo(wrap2);
+    availableBox.appendTo(wrap2);
+    wrap2.appendTo(this.content);
+
+}
 
 
