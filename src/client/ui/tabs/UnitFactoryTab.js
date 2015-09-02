7109e1f9f09410c91dbb92c556c5878bd4bdaa7e
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2015-09-02 16:44:23 +0200

    small bug fixes, including range feature

--- src/client/ui/tabs/UnitFactoryTab.js
+++ src/client/ui/tabs/UnitFactoryTab.js
@@ -12,13 +12,13 @@ var UnitFactoryTab = function (mapObj) {
 UnitFactoryTab.prototype.createUnits = function () {
     var self = this;
     var wrap1 = $('<div></div>').css({'display': 'inline-block','padding-right':'20px'});
-    var creationTitle =  $('<div>Recruitment</div>').css({});
-    var creationBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
+    this.creationTitle =  $('<div>Recruitment</div>').css({});
+    this.creationBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
     var allowedItemIds= game.objectTypes.get(this.mapObj.objTypeId)._initProperties._itemIds;
 
 
-    for (var i = 1; i<allowedItemIds.length; i++){
-        var itemId = allowedItemIds[i-1];
+    for (var i = 0; i<allowedItemIds.length; i++){
+        var itemId = allowedItemIds[i];
         var itemType =  game.itemTypes.get(itemId);
         var spritesheet = game.spritesheets.get(itemType._iconSpritesheetId);
         var spriteFrameIcon = spritesheet.frames[itemType._iconSpriteFrame];
@@ -33,21 +33,29 @@ UnitFactoryTab.prototype.createUnits = function () {
         var image = $('<div style="white-space:nowrap" ></div>')
         image.css({'background-image': 'url('+img+')' ,'background-position-x':-x, 'background-position-y':-y,'background-repeat':'no-repeat','width':breite,'height':hoehe});
         image.appendTo(container);
-        container.appendTo(creationBox);
-        var self = this;
-        container.click(function (e) {
-            //e.stopImmediatePropagation();
-            // e.preventDefault();
-            var tempId = "tempID"+Math.random();
-            var item = new ItemModel(game,{_id: tempId,_objectId:self.mapObj._id,_itemTypeId:itemId,_mapId:uc.layer.mapId})
-            var evt = new BuildItemEvent(game);
-            evt.setItem(item);
-            uc.addEvent(evt);
-        });
+
+        this.bindClickEvent(container,itemId)
+
     }
-    creationTitle.appendTo(wrap1);
-    creationBox.appendTo(wrap1);
+    this.creationTitle.appendTo(wrap1);
+    this.creationBox.appendTo(wrap1);
     wrap1.appendTo(this.content);
+
+}
+
+UnitFactoryTab.prototype.bindClickEvent = function (container,itemId) {
+    var self = this;
+    container.click(function (e) {
+        //e.stopImmediatePropagation();
+        // e.preventDefault();
+        var tempId = "tempID"+Math.random();
+        var item = new ItemModel(game,{_id: tempId,_objectId:self.mapObj._id,_itemTypeId:itemId,_mapId:uc.layer.mapId})
+        var evt = new BuildItemEvent(game);
+        evt.setItem(item);
+        uc.addEvent(evt);
+    });
+    container.appendTo(this.creationBox);
+    container = null;
 
 }
 
