6497a5ecec486f332aa75c2006f40c8c80f13550
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2016-06-30 23:01:54 +0200

    bug fix in Feature.js, starting activation item

--- src/client/ui/tabs/UpgradesTab.js
+++ src/client/ui/tabs/UpgradesTab.js
@@ -30,13 +30,29 @@ UpgradesTab.prototype.listAvailableUpgrades = function () {
         var img = spritesheet.images[spriteFrameIcon[4]];
         var gap = 8;
 
-        var container= $('<div style="white-space:nowrap"></div>').css({'width':breite, 'height':hoehe, 'position': 'relative','display': 'inline-block','padding-right':gap+'px'});
-        var image = $('<div style="white-space:nowrap" ></div>')
+        var container= $('<div style="white-space:nowrap" ></div>').css({'width':breite, 'height':hoehe, 'position': 'relative','display': 'inline-block','padding-right':gap+'px'});
+        var image = $('<div style="white-space:nowrap" ></div>');
         image.css({'background-image': 'url('+img+')' ,'background-position-x':-x, 'background-position-y':-y,'background-repeat':'no-repeat','width':breite,'height':hoehe});
         image.appendTo(container);
 
         this.buildUpgrade(container,itemId);
         container.css('cursor', 'pointer');
+        container.appendTo(this.creationBox);
+
+        // check which properties will be changed to on hover
+        /**
+         for (var i = 0; i<itemType._blocks.Feature[0]._stack.length; i++){
+           if(Object.keys(itemType._blocks.Feature[0]._stack[i])[0]=="AddToProp"){
+               var varnames = itemType._blocks.Feature[0]._stack[i].AddToProp.vars;
+               var varvalues = itemType._blocks.Feature[0]._stack[i].AddToProp.values;
+               var varoperator = itemType._blocks.Feature[0]._stack[i].AddToProp.operators;
+           }
+        }
+
+        $(container).tooltip({
+            content: "Awesome title!"
+        });
+         **/
     }
     this.creationTitle.appendTo(wrap1);
     this.creationBox.appendTo(wrap1);
@@ -47,15 +63,17 @@ UpgradesTab.prototype.listAvailableUpgrades = function () {
 UpgradesTab.prototype.listProducedUpgrades = function () {
     var self = this;
     var wrap2 = $('<div></div>').css({'display': 'inline-block','padding-right':'20px','top':'200','position': 'absolute'});
-    var availableTitle =  $('<div>Produced Upgrades</div>').css({});
-    var availableBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
+    this.availableTitle =  $('<div>Produced Upgrades</div>').css({});
+    this.availableBox =  $('<div></div>').css({'display': 'inline-block','border':'1px solid blue','width':160, 'height':130, 'position': 'relative','white-space':'pre-line'});
     var allItems= this.mapObj.getItems();
 
     for (var itemId in allItems) {
         if (allItems.hasOwnProperty(itemId)) {
             var item = allItems[itemId];
             var level = allItems[itemId].getLevel();
-            var maxLevel = item._itemType._maxLevel;
+            if (item._itemType._blocks.hasOwnProperty("Feature")){
+                var maxLevel = item._itemType._blocks.Feature.length;
+            }
             var itemType = allItems[itemId]._itemType;
             var spritesheet = game.spritesheets.get(itemType._iconSpritesheetId);
             var spriteFrameIcon = spritesheet.frames[itemType._iconSpriteFrame];
@@ -83,40 +101,45 @@ UpgradesTab.prototype.listProducedUpgrades = function () {
                 'height': hoehe
             });
             image.appendTo(iconContainer);
+
             // check if item can be activated
+            var spell = $('<div style="white-space:nowrap" ></div>');
             if(item._blocks.Feature._processedStack[item._blocks.Feature._executeIndex]==false) {
-                this.activatePerClick(iconContainer, item);
+                this.activatePerClick(spell, item);
                 iconContainer.css('cursor', 'pointer');
             }
+            spell.appendTo(iconContainer);
 
-            var upgradeContainer = $('<div><b>+</b></div>').css({
-                'width': 6,
-                'height': 6,
-                'position': 'relative',
-                'display': 'inline-block',
-                'left': 32 + 'px',
-                'top': -32 + 'px'
-            });
-            if (level < maxLevel) {
-                this.levelUpgrade(upgradeContainer, item);
-                upgradeContainer.css('cursor', 'pointer');
-            }
-
-            upgradeContainer.appendTo(iconContainer);
             var levelShowContainer = $('<div><b>' + level + '</b></div>').css({
                 'width': 6,
                 'height': 6,
                 'position': 'relative',
                 'display': 'inline-block',
-                'left': 27 + 'px',
+                'left': 32 + 'px',
                 'top': -14 + 'px'
             });
             levelShowContainer.appendTo(iconContainer);
-            iconContainer.appendTo(availableBox);
+
+            if (level < maxLevel) {
+                var upgradeContainer = $('<div><b>+</b></div>').css({
+                    'width': 6,
+                    'height': 6,
+                    'position': 'relative',
+                    'display': 'inline-block',
+                    'left': 27 + 'px',
+                    'top': -32 + 'px'
+                });
+
+                this.levelUpgrade(upgradeContainer, item);
+                upgradeContainer.css('cursor', 'pointer');
+                upgradeContainer.appendTo(iconContainer);
+            }
+
+            iconContainer.appendTo(this.availableBox);
         }
     }
-    availableTitle.appendTo(wrap2);
-    availableBox.appendTo(wrap2);
+    this.availableTitle.appendTo(wrap2);
+    this.availableBox.appendTo(wrap2);
     wrap2.appendTo(this.content);
 };
 
@@ -128,9 +151,6 @@ UpgradesTab.prototype.buildUpgrade = function (container,itemTypeId) {
         evt.setParentObject(self.mapObj);
         uc.addEvent(evt);
     });
-    container.appendTo(this.creationBox);
-    container = null;
-
 };
 
 UpgradesTab.prototype.levelUpgrade = function (container,item) {
@@ -138,9 +158,6 @@ UpgradesTab.prototype.levelUpgrade = function (container,item) {
     container.click(function (e) {
         self.mapObj._blocks.UpgradeProduction.levelUpgrade(item);
     });
-    container.appendTo(this.creationBox);
-    container = null;
-
 };
 
 
@@ -148,7 +165,4 @@ UpgradesTab.prototype.activatePerClick = function (container,item) {
     container.click(function (e) {
         item._blocks.Feature.checkStackExecution(true);
     });
-    container.appendTo(this.creationBox);
-    container = null;
-
 };
